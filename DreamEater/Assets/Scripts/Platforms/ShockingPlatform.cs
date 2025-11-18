a0c4005ba0262c1201d8ad1cebd19fddd077e248
trudytru30 <157128336+trudytru30@users.noreply.github.com>
2025-11-18 10:18:32 +0100

    shocking platform controller

--- DreamEater/Assets/Scripts/Platforms/ShockingPlatform.cs
+++ DreamEater/Assets/Scripts/Platforms/ShockingPlatform.cs
@@ -5,44 +5,25 @@ using UnityEngine;
 
 public class ShockingPlatform : MonoBehaviour
 {
-    [SerializeField] private float changeTime;
-    [SerializeField] private ShockingPlatform[] shockingPlatforms;
-    private ShockingPlatform currentShockingPlatform;
-    private int indexShockPlatform;
-
-    private void ChangeShock()
-    {
-        for (int i = 0; i < shockingPlatforms.Length; i++)
-        {
-            indexShockPlatform = i;
-            currentShockingPlatform = shockingPlatforms[indexShockPlatform];
-        }
-
-        if (indexShockPlatform == shockingPlatforms.Length)
-        {
-            indexShockPlatform = 0;
-        }
-    }
-
-    IEnumerator TimeTilChange()
-    {
-        yield return new WaitForSeconds(changeTime);
-    }
-
-    private void Update()
-    {
-        ChangeShock();
-        StartCoroutine(TimeTilChange());
-    }
-
+    private bool canShock = false;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (this.gameObject == currentShockingPlatform.gameObject) //si la plataforma es la que electrocuta entra al if
+        if (other.CompareTag("Player") && canShock) //comprueba que sea el player y que la plataforma este activa
         {
-            if (other.CompareTag("Player"))
-            {
-                other.GetComponent<Player>().Die(); //mata al player
-            }
+            other.GetComponent<Player>().Die(); //mata al player
         }
+
+        return;
+    }
+
+    public void setCanShock(bool _canShock)
+    {
+        canShock = _canShock;
+    }
+
+    public bool getCanShock()
+    {
+        return canShock;
     }
 }
