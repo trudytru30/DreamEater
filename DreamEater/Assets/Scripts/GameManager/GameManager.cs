0d001897a8567f59ea7f30d401667290082f3279
MiguelilloElPillo <mialca134@gmail.com>
2025-12-09 09:53:49 +0100

    Actualizacion GameManager

--- DreamEater/Assets/Scripts/GameManager/GameManager.cs
+++ DreamEater/Assets/Scripts/GameManager/GameManager.cs
@@ -1,4 +1,6 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -18,6 +20,20 @@ public class GameManager : MonoBehaviour
 
     #endregion
 
+    #region puertas
+    public int puertasDesbloqueadas;
+    public string nombreNivelPuertas;
+    [SerializeField] private GameObject _puerta1;
+    [SerializeField] private GameObject _puerta2;
+    [SerializeField] private GameObject _puerta3;
+    [SerializeField] private GameObject _puerta4;
+    #endregion
+
+    private void Start()
+    {
+        puertasDesbloqueadas = 1;
+    }
+
     private void Awake()
     {
         if (Instance == null)
@@ -29,5 +45,47 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        ActivatePuertas();
+    }
+
+    public void ActivatePuertas()
+    {
+        var actualScene = SceneManager.GetActiveScene();
+        if (actualScene.name != nombreNivelPuertas)
+        return;
+
+        _puerta1.SetActive(false);
+        _puerta2.SetActive(false);
+        _puerta3.SetActive(false);
+        _puerta4.SetActive(false);
+
+        switch (puertasDesbloqueadas)
+        {
+            case 1:
+                _puerta1.SetActive(true);
+                break;
+            case 2:
+                _puerta1.SetActive(true);
+                _puerta2.SetActive(true);
+                break;
+            case 3:
+                _puerta1.SetActive(true);
+                _puerta2.SetActive(true);
+                _puerta3.SetActive(true);
+                break;
+            case 4:
+                _puerta1.SetActive(true);
+                _puerta2.SetActive(true);
+                _puerta3.SetActive(true);
+                _puerta4.SetActive(true);
+                break;
+            default:
+                break;
+
+        }
     }
 }
