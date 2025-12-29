19d9ded2398460ffe88fbda94d0cdad07db1c1a3
MiguelilloElPillo <mialca134@gmail.com>
2025-12-29 18:00:25 +0100

    Arreglado Animators Level Limbo

--- DreamEater/Assets/Scripts/GameManager/ActivatePuertas.cs
+++ DreamEater/Assets/Scripts/GameManager/ActivatePuertas.cs
@@ -17,6 +17,11 @@ public class ActivatePuertas : MonoBehaviour
     [SerializeField] private GameObject _trigger_4;
     [SerializeField] private GameObject _trigger_5;
 
+    [Header("Animators")]
+    [SerializeField] private Animator[] puertaanim;
+
+
+
 
     private void Start()
     {
@@ -32,28 +37,31 @@ public class ActivatePuertas : MonoBehaviour
     {
         _puertasDesbloqueadas = GameManager.Instance.puertasDesbloqueadas;
 
-        _trigger_1.SetActive(false);
-        _trigger_2.SetActive(false);
-        _trigger_3.SetActive(false);
-        _trigger_4.SetActive(false);
-        _trigger_5.SetActive(false);         
+        DeactivateTrigger();
+        DeactivateAnimators();
 
         switch (_puertasDesbloqueadas)
         {
             case 1:
                 _puerta1.SetActive(true);
                 _trigger_1.SetActive(true);
+                puertaanim[0].enabled = true;
+                puertaanim[1].enabled = true;
                 break;
             case 2:
                 _puerta2.SetActive(true);
                 _puerta1.SetActive(true);
                 _trigger_2.SetActive(true);
+                puertaanim[2].enabled = true;
+                puertaanim[3].enabled = true;
                 break;
             case 3:
                 _puerta3.SetActive(true);
                 _puerta2.SetActive(true);
                 _puerta1.SetActive(true);
                 _trigger_3.SetActive(true);
+                puertaanim[4].enabled = true;
+                puertaanim[5].enabled = true;
                 break;
             case 4:
                 _puerta4.SetActive(true);
@@ -61,6 +69,8 @@ public class ActivatePuertas : MonoBehaviour
                 _puerta2.SetActive(true);
                 _puerta1.SetActive(true);
                 _trigger_4.SetActive(true);
+                puertaanim[6].enabled = true;
+                puertaanim[7].enabled = true;
                 break;
             case 5:
                 _puerta5.SetActive(true);
@@ -69,9 +79,28 @@ public class ActivatePuertas : MonoBehaviour
                 _puerta2.SetActive(true);
                 _puerta1.SetActive(true);
                 _trigger_5.SetActive(true);
+                puertaanim[8].enabled = true;
+                puertaanim[9].enabled = true;
                 break;
             default:
                 break;
         }
+    }
+
+    private void DeactivateTrigger()
+    {
+        _trigger_1.SetActive(false);
+        _trigger_2.SetActive(false);
+        _trigger_3.SetActive(false);
+        _trigger_4.SetActive(false);
+        _trigger_5.SetActive(false);
+    }
+    private void DeactivateAnimators()
+    {
+        for(int i = 0; i<puertaanim.Length; i++)
+        {
+            puertaanim[i].enabled = false;
+        }
+
     }
 }
