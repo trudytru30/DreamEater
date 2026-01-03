32a8205ec720647716b33d9df6232cc056335f06
MiguelilloElPillo <mialca134@gmail.com>
2026-01-03 16:15:46 +0100

    Lvl_4 Update

---
+++ DreamEater/Assets/Scripts/Levels/FallObjects.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+
+[RequireComponent(typeof(BoxCollider))]
+public class FallObjects : MonoBehaviour
+{
+    [SerializeField] private Rigidbody[] objects;
+    [SerializeField] private GameObject blackmask;
+    
+
+    private void Start()
+    {
+        for(int i = 0; i < objects.Length; i++)
+        {
+            objects[i].useGravity = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            blackmask.SetActive(false);
+            for(int i = 0; i < objects.Length; i++)
+            {
+                objects[i].useGravity = true;
+            }
+        }
+    }
+
+}
