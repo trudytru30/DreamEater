6cf5198d2f738357f763d9de8f372d8789f75f1d
MiguelilloElPillo <mialca134@gmail.com>
2026-01-05 18:21:37 +0100

    Level_4 Update

--- DreamEater/Assets/Scripts/BackGround/BGFollowPlayer.cs
+++ DreamEater/Assets/Scripts/BackGround/BGFollowPlayer.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class BGFollowPlayer : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private float verticalOffset;
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x, player.position.y+verticalOffset, transform.position.z);
     }
 }
