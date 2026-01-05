a85f8bd908fa5c754523bc7c0d4e6eeaca85f381
MiguelilloElPillo <mialca134@gmail.com>
2026-01-05 16:27:37 +0100

    Level_4 Update

--- DreamEater/Assets/Scripts/Levels/LevelManager.cs
+++ DreamEater/Assets/Scripts/Levels/LevelManager.cs
@@ -43,12 +43,9 @@ public class LevelManager : MonoBehaviour
         }
         else
         {
-            if (CheckLevel4Completed()) //Comprueba si es el nivel 4 y si lo es, comprueba que halla cogido los pickups
-            {
-                GameManager.Instance.ChangeNextLevelLoading(nextScene);
-                GameManager.Instance.ChangePuertasDesbloqueadas(nivelDesbloqueado);
-                SceneManager.LoadScene(_sceneName);
-            }
+            GameManager.Instance.ChangeNextLevelLoading(nextScene);
+            GameManager.Instance.ChangePuertasDesbloqueadas(nivelDesbloqueado);
+            SceneManager.LoadScene(_sceneName);
         }
     }
 
@@ -60,27 +57,6 @@ public class LevelManager : MonoBehaviour
         }
     }
 
-    private bool CheckLevel4Completed()
-    {
-        Scene actualscene = SceneManager.GetActiveScene();
-
-        if(actualscene.name == "Level_4")
-        {
-            if (PickUpUISHow.Instance._counter >= PickUpUISHow.Instance._maxPickups)
-            {
-                return true;
-            }
-            else
-            {
-                StartCoroutine(PickUpUISHow.Instance.MustPickUpAll());
-                return false;
-            }
-        }
-        else
-        {
-            return true;
-        }
-    }
 
 
 }
