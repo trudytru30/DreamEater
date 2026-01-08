4e13b762654af6322428876d2c3159ff797f13ca
MiguelilloElPillo <mialca134@gmail.com>
2026-01-08 22:42:11 +0100

    Update Activate Puertas

--- DreamEater/Assets/Scripts/GameManager/ActivatePuertas.cs
+++ DreamEater/Assets/Scripts/GameManager/ActivatePuertas.cs
@@ -10,6 +10,12 @@ public class ActivatePuertas : MonoBehaviour
     [SerializeField] private GameObject _puerta4;
     [SerializeField] private GameObject _puerta5;
 
+    [Header("Bichos")]
+    [SerializeField] private GameObject _bicho1;
+    [SerializeField] private GameObject _bicho2;
+    [SerializeField] private GameObject _bicho3;
+    [SerializeField] private GameObject _bicho4;
+
     [Header("Triggers")]
     [SerializeField] private GameObject _trigger_1;
     [SerializeField] private GameObject _trigger_2;
@@ -37,6 +43,7 @@ public class ActivatePuertas : MonoBehaviour
 
         DeactivateTrigger();
         DeactivateAnimators();
+        DeactivateBichos();
 
         switch (_puertasDesbloqueadas)
         {
@@ -45,6 +52,7 @@ public class ActivatePuertas : MonoBehaviour
                 _trigger_1.SetActive(true);
                 puertaanim[0].enabled = true;
                 puertaanim[1].enabled = true;
+                _bicho1.SetActive(true);
                 break;
             case 2:
                 _puerta2.SetActive(true);
@@ -52,6 +60,8 @@ public class ActivatePuertas : MonoBehaviour
                 _trigger_2.SetActive(true);
                 puertaanim[2].enabled = true;
                 puertaanim[3].enabled = true;
+                _bicho1.SetActive(true);
+                _bicho2.SetActive(true);
                 break;
             case 3:
                 _puerta3.SetActive(true);
@@ -60,6 +70,9 @@ public class ActivatePuertas : MonoBehaviour
                 _trigger_3.SetActive(true);
                 puertaanim[4].enabled = true;
                 puertaanim[5].enabled = true;
+                _bicho1.SetActive(true);
+                _bicho2.SetActive(true);
+                _bicho3.SetActive(true);
                 break;
             case 4:
                 _puerta4.SetActive(true);
@@ -69,6 +82,10 @@ public class ActivatePuertas : MonoBehaviour
                 _trigger_4.SetActive(true);
                 puertaanim[6].enabled = true;
                 puertaanim[7].enabled = true;
+                _bicho1.SetActive(true);
+                _bicho2.SetActive(true);
+                _bicho3.SetActive(true);
+                _bicho4.SetActive(true);
                 break;
             case 5:
                 _puerta5.SetActive(true);
@@ -100,5 +117,12 @@ public class ActivatePuertas : MonoBehaviour
             puertaanim[i].enabled = false;
         }
 
+    }
+    private void DeactivateBichos()
+    {
+        _bicho1.SetActive(false);
+        _bicho2.SetActive(false);
+        _bicho3.SetActive(false);
+        _bicho4.SetActive(false);
     }
 }
